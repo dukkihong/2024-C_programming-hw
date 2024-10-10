9125cd9604de785065bf6e4f577325ad78dd684d
dukkihong <127312253+dukkihong@users.noreply.github.com>
2024-10-10 23:27:38 +0900

    Add files via upload

---
+++ hw4.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int num;
+	int i;
+	printf("Please enter a number: ");
+	scanf("%d", &num);
+	for (i = 2; i < num; i++) {
+		if (num % i == 0) {
+			break;
+		}
+	}
+	if (i == num) {
+		printf("It is a prime number. \n");
+	}
+	else {
+		printf("It is not a prime number. \n");
+	}
+}
